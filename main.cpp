1dbaa7b3c5b4b77a1b16c37b8c196021b98a3a55
persja40 <persja40@gmail.com>
2018-04-30 15:30:39 +0200

    broadcast data

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <numeric>
 #include <vector>
+#include <future>
 
 using namespace std;
 
@@ -12,13 +13,13 @@ int main(int argc, char *argv[])
 {
     // setup UPC++ runtime
     upcxx::init();
-    vector<int> data_to_sort{};
-    int data_to_sort_size;
     int numprocs = upcxx::rank_n();
     int myid = upcxx::rank_me();
 
     // PHASE I
     // read data from file
+    upcxx::global_ptr<int> global_data_size = nullptr;
+    upcxx::global_ptr<int> global_data = nullptr;
     if (myid == 0)
     {
         string input_file;
@@ -27,6 +28,7 @@ int main(int argc, char *argv[])
         else
             input_file = "example.txt";
         ifstream ifile;
+        vector<int> data_to_sort{};
         ifile.open(input_file);
         while (!ifile.eof())
         {
@@ -35,16 +37,28 @@ int main(int argc, char *argv[])
             data_to_sort.push_back(t);
         }
         ifile.close();
-        data_to_sort_size = data_to_sort.size();
+        global_data_size = upcxx::new_<int>(data_to_sort.size());
+        global_data = upcxx::new_array<int>(data_to_sort.size());
+        auto iter = global_data;
+        for (int i = 0; i < data_to_sort.size(); i++)
+        {
+            upcxx::rput(data_to_sort[i], iter).wait();
+            iter++;
+        }
     }
+    global_data_size = upcxx::broadcast(global_data_size, 0).wait();
+    global_data = upcxx::broadcast(global_data, 0).wait();
+
+    // PHASE II
 
     //COUT TEST SECTION
     if (myid == 0)
         cout << "liczba wątków: " << numprocs << endl;
-
-    for (const auto &e : data_to_sort)
-        cout << e << " ";
-    cout << endl;
+    if (myid == 2){
+        auto fut = rget ( global_data_size );
+        fut.wait();
+        cout<<"Ilosc elem: "<<fut.result()<<endl;
+    }
     // close down UPC++ runtime
     upcxx::finalize();
     return 0;
