f35988026f4980b8a47d0e2d64ccc4c4cd9a5096
persja40 <persja40@gmail.com>
2018-04-30 14:19:25 +0200

    read data from file

--- main.cpp
+++ main.cpp
@@ -1,15 +1,50 @@
 #include <upcxx/upcxx.hpp>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // setup UPC++ runtime
     upcxx::init();
+    vector<int> data_to_sort{};
+    int data_to_sort_size;
+    int numprocs = upcxx::rank_n();
+    int myid = upcxx::rank_me();
 
-    // upcxx::rank_me() - get number for this rank
-    cout << "Hello world from rank " << upcxx::rank_me() << endl;
+    // PHASE I
+    // read data from file
+    if (myid == 0)
+    {
+        string input_file;
+        if (argc == 2)
+            input_file = argv[1];
+        else
+            input_file = "example.txt";
+        ifstream ifile;
+        ifile.open(input_file);
+        while (!ifile.eof())
+        {
+            int t;
+            ifile >> t;
+            data_to_sort.push_back(t);
+        }
+        ifile.close();
+        data_to_sort_size = data_to_sort.size();
+    }
 
+    //COUT TEST SECTION
+    if (myid == 0)
+        cout << "liczba wątków: " << numprocs << endl;
+
+    for (const auto &e : data_to_sort)
+        cout << e << " ";
+    cout << endl;
     // close down UPC++ runtime
     upcxx::finalize();
     return 0;
